0cb707ddeeb70b19614dbd52cbe9575b4cb0a728
Nguyễn Hồng Ân <136869338+NguynHngAn@users.noreply.github.com>
2024-11-24 15:30:59 +0700

    Update ShoppingCart.cs

--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -17,6 +17,7 @@ namespace BulkyBook.Models {
         public Product Product { get; set; }
         [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
+	public string Description { get; set; }
         public string ImageUrl { get; set; }
         public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
